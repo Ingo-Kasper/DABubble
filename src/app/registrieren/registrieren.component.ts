74a1e1c6b4062451e31a367b4bbd280d6c32a6c9
Ingo Kasper <Djindustrie@Protonmail.com>
2025-01-15 09:39:32 +0100

    test

---
+++ src/app/registrieren/registrieren.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrieren',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrieren.component.html',
+  styleUrl: './registrieren.component.scss'
+})
+export class RegistrierenComponent {
+
+}
