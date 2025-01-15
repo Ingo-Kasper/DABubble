ebc751a3fd859536d2e4ad431583543579e76cf1
Ingo Kasper <Djindustrie@Protonmail.com>
2025-01-15 10:07:03 +0100

    Add html & ts, fb noch nicht inject

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  /**
+   * @fb muss noch inject werden und schauen was genau das macht
+   */
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  router = inject(Router);
 
+  form =this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onSubmit() {
+    console.log('submit');
+  }
 }
