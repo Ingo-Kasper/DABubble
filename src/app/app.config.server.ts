d6ed4cea34d4d03281a47b4bb82436b3d8c38b5a
Ingo Kasper <Djindustrie@Protonmail.com>
2025-01-19 00:28:14 +0100

    app.config.ts commit

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,15 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRendering } from '@angular/platform-server'; // Unterstützt serverseitiges Rendering (SSR)
+
+// Importiert die allgemeine Konfiguration der Anwendung aus einer separaten Datei
 import { appConfig } from './app.config';
 
+// Definiert spezifische Konfigurationen für die serverseitige Anwendung
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering() // Fügt Anbieter für serverseitiges Rendering hinzu
   ]
 };
 
+// Kombiniert die allgemeine Anwendungskonfiguration mit der serverseitigen Konfiguration
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideFirebaseApp, initializeApp  } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0sL9Yy4C9phaBc8FXOxcJb2SmEa3OkLo",
@@ -12,16 +12,15 @@ const firebaseConfig = {
   projectId: "dabubble-projekt-von-kasper",
   storageBucket: "dabubble-projekt-von-kasper.firebasestorage.app",
   messagingSenderId: "264595788933",
-  appId: "1:264595788933:web:97aada3641ca4a1b4c05ec"
+  appId: "1:264595788933:web:97aada3641ca4a1b4c05ec",
 };
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideClientHydration(),
-  importProvidersFrom([
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth())
-  ]),
-],
+    provideAuth(() => getAuth()),
+  ],
 };
