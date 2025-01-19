b0d2ba340e3210b9d85f6e93bfeab2961fa81940
Ingo Kasper <Djindustrie@Protonmail.com>
2025-01-19 14:44:03 +0100

    add RouterLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core'; // Basis-Dekorator für Angular-Komponenten
-import { RouterOutlet } from '@angular/router'; // Erlaubt das Einbinden von Routeninhalten in die Komponente
+import { RouterLink, RouterOutlet } from '@angular/router'; // Erlaubt das Einbinden von Routeninhalten in die Komponente
 
 @Component({
   selector: 'app-root', // Der HTML-Tag, der diese Komponente repräsentiert
   standalone: true, // Standalone-Komponenten benötigen kein Angular-Modul (ab Angular 14 möglich)
-  imports: [RouterOutlet], // Importiert zusätzliche Angular-Module für diese Komponente, hier RouterOutlet für Routing
+  imports: [RouterOutlet, RouterOutlet, RouterLink], // Importiert zusätzliche Angular-Module für diese Komponente, hier RouterOutlet für Routing
   templateUrl: './app.component.html', // Verweist auf die HTML-Vorlage der Komponente
   styleUrl: './app.component.scss' // Verweist auf die CSS-Datei der Komponente
 })
+
 export class AppComponent {
   // Titel der Anwendung, kann in der Komponente oder im Template verwendet werden
   title = 'DABubble';
